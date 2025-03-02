97f384380c8eed659413683818130a268985dfe2
Serezha1993 <007serzh2014@mail.ru>
2025-03-02 11:51:55 +0300

    deploy

--- src/Components/Generate/QrCodeGenerator.jsx
+++ src/Components/Generate/QrCodeGenerator.jsx
@@ -1,41 +1,92 @@
-import {useState} from 'react'
-import { QRCodeSVG } from "qrcode.react";
-import s from'./qrCodeGenerator.module.css';
+// import {useState} from 'react'
+// import { QRCodeSVG } from "qrcode.react";
+// import s from'./qrCodeGenerator.module.css';
+
+// export const QrCodeGenerator = () => {
+//     const [value, setValue] = useState('Hello');
+//     const [result, setResult] = useState('');
+
+//     const onClickHandler = (event) => {
+//         setResult(value);
+//         setValue('');
+       
+//     };
+
+//     const OnChangeHandler = (event) => {
+//         setValue(event.target.value)
+//         setResult('');
+//     };
+//     console.log('result', result);
+
+//     return (
+//         <div className={s.container}>
+//             <input 
+//               type="text"
+//               value={value} 
+//               placeholder='Введите текст...'
+//               onChange={OnChangeHandler}
+//               className={s.input}
+//               />
+//             <button type="button" className={s.button} onClick={onClickHandler}>
+//                 Сгенерировать QR
+//             </button>
+
+//             {result !== '' && (
+//                 <div className={s.qrWrapper}>
+//                   <QRCodeSVG value={result} size={200}/>
+//                 </div>
+//                 )}
+//         </div>
+//     );
+// };
+
+
+import { useState } from 'react';
+import { QRCodeSVG } from 'qrcode.react';
+
+import { GENERATE_DATA } from '../../constants';
+
+import s from './qrCodeGenerator.module.css';
 
 export const QrCodeGenerator = () => {
-    const [value, setValue] = useState('Hello');
+    const [value, setValue] = useState('');
     const [result, setResult] = useState('');
 
-    const onClickHandler = (event) => {
+    const onClickHandler = () => {
+        const prevData = JSON.parse(localStorage.getItem(GENERATE_DATA) || '[]');
+
+        localStorage.setItem(
+            GENERATE_DATA,
+            JSON.stringify([...prevData, value])
+        );
+
         setResult(value);
         setValue('');
-       
     };
 
-    const OnChangeHandler = (event) => {
-        setValue(event.target.value)
+    const onChangeHandler = (event) => {
+        setValue(event.target.value);
         setResult('');
     };
-    console.log('result', result);
 
     return (
         <div className={s.container}>
-            <input 
-              type="text"
-              value={value} 
-              placeholder='Введите текст...'
-              onChange={OnChangeHandler}
-              className={s.input}
-              />
+            <input
+                type="text"
+                value={value}
+                placeholder="Введите текст..."
+                onChange={onChangeHandler}
+                className={s.input}
+            />
             <button type="button" className={s.button} onClick={onClickHandler}>
                 Сгенерировать QR
             </button>
 
             {result !== '' && (
                 <div className={s.qrWrapper}>
-                  <QRCodeSVG value={result} size={200}/>
-                </div>
-                )}
+                    <QRCodeSVG value={result} size={200} />
+                </div> 
+            )}
         </div>
     );
 };
--- src/Components/Scan/QrCodeScanner.jsx
+++ src/Components/Scan/QrCodeScanner.jsx
@@ -4,32 +4,64 @@ import s from './qrCodeScanner.module.css';
 
 import {SCAN_DATA} from '../../constants';
 
+// export const QrCodeScanner = () => {
+//     const [scanned, setScanned] = useState(null);
+
+//     const scanHandler = (result) => {
+//         setScanned(result[0].rawValue);
+
+//         const prevData = JSON.parse(localStorage.getItem(SCAN_DATA) || []);
+//         console.log('svsv', prevData);
+//         localStorage.setItem('SCAN_DATA', JSON.stringify([result[0].rawValue]));
+//     };
+    
+//     return (
+//         <div className={s.container}>
+//             <p>{scanned}</p>
+//             <Scanner 
+//             allowMultiple 
+//             onScan={scanHandler}
+//             components={{
+//                 audio: false,
+//                 finder: false,
+//             }}
+//             styles={{
+//                 container:{width: 350}
+//             }}
+//             />
+//             <p className={s.result}>{scanned}</p>
+//         </div>
+//     )
+// };
+
 export const QrCodeScanner = () => {
     const [scanned, setScanned] = useState(null);
 
     const scanHandler = (result) => {
-        setScanned(result[0].rawValue);
+        if (!result) return;
 
-        const prevData = JSON.parse(localStorage.getItem(SCAN_DATA) || []);
-        console.log('svsv', prevData);
-        localStorage.setItem('SCAN_DATA', JSON.stringify([result[0].rawValue]));
+        const prevData = JSON.parse(localStorage.getItem(SCAN_DATA) || '[]');
+
+        if (prevData.includes(result.text)) return;
+
+        setScanned(result.text);
+
+        localStorage.setItem(
+            SCAN_DATA,
+            JSON.stringify([...prevData, result.text])
+        );
     };
-    
+
     return (
         <div className={s.container}>
-            <p>{scanned}</p>
-            <Scanner 
-            allowMultiple 
-            onScan={scanHandler}
-            components={{
-                audio: false,
-                finder: false,
-            }}
-            styles={{
-                container:{width: 350}
-            }}
+            <QrReader
+                constraints={{ facingMode: 'environment' }}
+                scanDelay={1000}
+                onResult={scanHandler}
+                containerStyle={{ width: '500px' }}
             />
+        
             <p className={s.result}>{scanned}</p>
         </div>
-    )
+    );
 };
